8a528eee000cee0ed9f1d222a48fa8a96bf416ba
fadlirizaldy <fadli.rizaldi12@gmail.com>
2024-07-10 22:49:40 +0700

    fix: button close in modal

--- src/components/ModalNewThread.jsx
+++ src/components/ModalNewThread.jsx
@@ -28,11 +28,7 @@ const ModalNewThread = (props) => {
     toast.success("Posted!", { autoClose: 800, pauseOnHover: false });
   };
   return (
-    <dialog
-      id="modalNewThread"
-      className="modal modal-open"
-      onSubmit={handleSubmit(onSubmit)}
-    >
+    <dialog id="modalNewThread" className="modal modal-open" onSubmit={handleSubmit(onSubmit)}>
       <div className="modal-box w-11/12 max-w-xl">
         <button
           className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
@@ -41,24 +37,10 @@ const ModalNewThread = (props) => {
           ✕
         </button>
         <h3 className="font-bold text-xl text-center">New Thread</h3>
-        <form
-          className="mt-5 flex flex-col gap-3"
-          onSubmit={handleSubmit}
-          key={"thread"}
-          method="input"
-        >
-          <InputText
-            errors={errors.title}
-            register={register}
-            {...title_validation}
-          />
+        <form className="mt-5 flex flex-col gap-3" onSubmit={handleSubmit} key={"thread"} method="input">
+          <InputText errors={errors.title} register={register} {...title_validation} />
 
-          <InputText
-            errors={errors.description}
-            register={register}
-            isTextArea
-            {...desc_validation}
-          />
+          <InputText errors={errors.description} register={register} isTextArea {...desc_validation} />
 
           <div className="flex justify-end">
             <button className="btn h-fit min-h-fit px-4 py-2 text-white bg-blue-400 text-base hover:bg-blue-500">
@@ -67,7 +49,6 @@ const ModalNewThread = (props) => {
           </div>
         </form>
       </div>
-      <button onClick={() => setIsShowModal(false)}>close</button>
     </dialog>
   );
 };
